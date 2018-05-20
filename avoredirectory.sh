4e6780a643fbd57a8efda017c13da3f22e9c75f3
valdeiltonfelix <javaibotirama@gmail.com>
2018-05-20 11:12:22 -0300

    Recomençando o turial
    	*Não funcionaou com esperado !

--- avoredirectory.sh
+++
@@ -1,6 +0,0 @@
-# !/bin/bash
-# Executa a avore de diretorio do turial do zendfremework
-# Author:Valdeilton felix
-
-
-mkdir -p "path/to/install/public/zf2-tutorial/module/Album/{config/,src/Album/{Controller,Form,Model}/,view/album/album}"
